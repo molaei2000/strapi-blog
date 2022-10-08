b0c1dbbbcd85d592a195ffafad91f30e49a019fa
mohammad mahdi <mahdi.molaei2000@gmail.com>
2022-10-08 11:20:23 +0330

    add clodinary config

--- config/middlewares.js
+++ config/middlewares.js
@@ -1,7 +1,20 @@
 module.exports = [
   'global::timer-header',
   'strapi::errors',
-  'strapi::security',
+  {
+    name: 'strapi::security',
+    config: {
+      contentSecurityPolicy: {
+        useDefaults: true,
+        directives: {
+          'connect-src': ["'self'", 'https:'],
+          'img-src': ["'self'", 'data:', 'blob:', 'dl.airtable.com', 'res.cloudinary.com'],
+          'media-src': ["'self'", 'data:', 'blob:', 'dl.airtable.com', 'res.cloudinary.com'],
+          upgradeInsecureRequests: null,
+        },
+      },
+    },
+  }, ,
   'strapi::cors',
   'strapi::poweredBy',
   'strapi::logger',
